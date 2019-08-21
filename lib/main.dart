db7ab2c6eed1eba229f8eee560ca0c7f22720dce
Fernando May <fmay130@gmail.com>
2019-08-21 10:05:03 -0500

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() {
-  final HttpLink httpLink = HttpLink(
+final HttpLink httpLink = HttpLink(
     uri: 'https://api.github.com/graphql',
   );
 
-  /*
   final AuthLink authLink = AuthLink(
-    getToken: () async => 'Bearer <TOKEN>',
+    getToken: () async => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
+    // OR
+    // getToken: () => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
   );
 
   final Link link = authLink.concat(httpLink);
-  */
+
   ValueNotifier<GraphQLClient> client = ValueNotifier(
     GraphQLClient(
       cache: InMemoryCache(),
-      link: httpLink,
+      link: link,
     ),
   );
 
-  runApp(MyApp(client));
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
-  final ValueNotifier<GraphQLClient> cliente = ValueNotifier(
-    GraphQLClient(
-      cache: InMemoryCache(),
-      link: HttpLink(uri: ""),
-    ),
-  );
-
-  MyApp(cliente);
-
   @override
   Widget build(BuildContext context) {
     return GraphQLProvider(
-      client: cliente,
+      client: client,
       child: CacheProvider(
         child: MaterialApp(
-          title: 'GraphQL Demo',
+          title: 'GraphicsQL',
         ),
       ),
     );
