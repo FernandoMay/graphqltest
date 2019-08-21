e7631de27c261e670783bfcde5b660f04b2b05f0
Fernando May <fmay130@gmail.com>
2019-08-21 10:32:55 -0500

    nullable

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
-final HttpLink httpLink = HttpLink(
-    uri: 'https://api.github.com/graphql',
-  );
-
-  final AuthLink authLink = AuthLink(
-    getToken: () async => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
-    // OR
-    // getToken: () => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
-  );
-
-  final Link link = authLink.concat(httpLink);
-
-  ValueNotifier<GraphQLClient> client = ValueNotifier(
-    GraphQLClient(
-      cache: InMemoryCache(),
-      link: link,
-    ),
-  );
-
 void main() {
   runApp(MyApp());
 }
@@ -27,13 +8,58 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GraphQLProvider(
-      client: client,
-      child: CacheProvider(
-        child: MaterialApp(
-          title: 'GraphicsQL',
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
       ),
+      home: CategoriesPage(),
+    );
+  }
+}
+
+class CategoriesPage extends StatelessWidget {
+  final List<dynamic> categories = [
+    {"title": "Category 1"},
+    {"title": "Category 2"},
+    {"title": "Category 3"},
+    {"title": "Category 4"},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Categories'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.person),
+            tooltip: 'Login',
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: CategoriesList(
+        categories: categories,
+      ),
+    );
+  }
+}
+
+class CategoriesList extends StatelessWidget {
+  const CategoriesList({Key key, @required this.categories}) : super(key: key);
+
+  final List categories;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: categories.length,
+      itemBuilder: (context, index) {
+        final category = categories[index];
+
+        return ListTile(title: Text(category['title']));
+      },
     );
   }
 }
